f294724ed89570b87a50d465e9a4ed3243c7f8cd
huypqh11 <121076944+huypqh11@users.noreply.github.com>
2023-09-28 17:50:54 +0700

    add typeproduct

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,20 @@
 import  React from 'react';
-import { Row } from 'antd';
+// import { Row } from 'antd';
+import TypeProduct from '../../components/TypeProduct/TypeProduct';
+import { WrapperTypeProduct } from './style';
 
 const HomePage = () => {
+    const arr=  ['TV','Tủ lạnh','Máy giặt','Điều hòa','Máy lạnh','Máy nước nóng','Máy rửa chén','Máy hút bụi','Máy sấy quần áo','Máy lọc không khí','Máy rửa chén','Máy hút bụi','Máy sấy quần áo','Máy lọc không khí']
     return (
-    <div>
-        <Row>
-        </Row>
+    <div style ={{padding:'0,120px'}} >
+        <WrapperTypeProduct>
+            {arr.map((item) => {
+                return (
+                    <TypeProduct name={item} key={item}/>
+                )
+            })}
+        </WrapperTypeProduct>
+        HomePage  
     </div>
     );
 };
---
+++ src/pages/HomePage/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+export const WrapperTypeProduct = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    justify-content: flex;
+`
